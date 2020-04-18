59c230ce9d49dc9619c03eaa6fa6961d4c66ca97
dermatobia <webblewebble@gmail.com>
2020-04-18 15:09:35 -0400

    add sequence array dynamically

--- drumMachine.js
+++ drumMachine.js
@@ -15,24 +15,29 @@ const MAP_DISPLAY = {
 };
 
 class SoundType {
-  constructor(type) {
+  constructor(type, activeStepIds = []) {
     this.type = type;
-    this.tempo = 128;
+    this.activeStepIds = activeStepIds;
+    this.tempo = 60;
     this.intervalId = null;
 
     // DOM Elements
     this.stepTemplate = document.getElementById('steps-template');
     this.stepWrapper = document.querySelector(`.step-wrapper.${this.type}`);
-    this.steps = [];
+    this.stepElements = [];
 
     this.mountElements();
   }
 
   mountElements() {
+    let dotHtmlCode = '&#8226;';
+
     // Append template clone to parent container and add title.
     this.stepWrapper.appendChild(this.stepTemplate.content.cloneNode(true));
     this.stepWrapper.firstElementChild.innerText = MAP_DISPLAY[this.type];
-    this.steps = this.stepWrapper.querySelectorAll('.step');
+    this.stepElements = this.stepWrapper.querySelectorAll('.step');
+    console.log('type', this.type);
+    this.activeStepIds.forEach(id => this.stepElements[id].innerHTML = dotHtmlCode);
   }
 
   startTempoAnimation() {
@@ -41,7 +46,7 @@ class SoundType {
 
     let indexCounter = 0;
     const stepInterval = 60/this.tempo*4/8*1000;  // in milliseconds
-    const stepsLength = this.steps.length;        // 16 in this case
+    const stepsLength = this.stepElements.length;        // 16 in this case
 
     this.intervalId = setInterval(() => {
       const currIndex = indexCounter % stepsLength;
@@ -51,8 +56,8 @@ class SoundType {
       prevIndex = prevIndex > -1 ? prevIndex : stepsLength - 1;
 
       // highlight and clear highlight step boxes
-      this.steps[currIndex].classList.add('active');
-      this.steps[prevIndex].classList.remove('active');
+      this.stepElements[currIndex].classList.add('highlight');
+      this.stepElements[prevIndex].classList.remove('highlight');
 
       // increment index counter
       indexCounter = (indexCounter + 1) % stepsLength;
@@ -61,27 +66,37 @@ class SoundType {
 
   stopTempoAnimation() {
     clearInterval(this.intervalId);
+    this.intervalId = null;
 
-    Array.from(this.steps).forEach(item => item.classList.remove('active'));
+    Array.from(this.stepElements).forEach(item => item.classList.remove('highlight'));
   }
 }
 
-let kick = new SoundType(TYPES.KICK);
-let snare = new SoundType(TYPES.SNARE);
-let openHat = new SoundType(TYPES.OPEN_HAT);
-let closedHat = new SoundType(TYPES.CLOSED_HAT);
+let arr0 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let arr1 = [1, 3, 5, 7, 9, 11, 13, 15];
+let arr2 = [0, 2, 4, 6, 8, 10, 12, 14];
+let arr3 = [3, 7, 11, 15];
+
+let kick = new SoundType(TYPES.KICK, arr1);
+let snare = new SoundType(TYPES.SNARE, arr2);
+let openHat = new SoundType(TYPES.OPEN_HAT, arr3);
+let closedHat = new SoundType(TYPES.CLOSED_HAT, arr0);
 
 let stopBtn = document.querySelector('.stop.btn');
 let playBtn = document.querySelector('.play.btn');
 
 function start() {
   kick.startTempoAnimation();
+  snare.startTempoAnimation();
   openHat.startTempoAnimation();
+  closedHat.startTempoAnimation();
 }
 
 function stop() {
   kick.stopTempoAnimation();
+  snare.stopTempoAnimation();
   openHat.stopTempoAnimation();
+  closedHat.stopTempoAnimation();
 }
 
 stopBtn.addEventListener('click', (e) => {
