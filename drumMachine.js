95c4c49cfbce08bcd9eff0dc12006fe755ca1bcf
dermatobia <webblewebble@gmail.com>
2020-04-18 15:45:08 -0400

    Timer extends StepSequencer

--- drumMachine.js
+++ drumMachine.js
@@ -6,7 +6,7 @@ const TYPES = {
   CLOSED_HAT: 'closed-hat'
 }
 
-const MAP_DISPLAY = {
+const MAP_DISPLAY_NAME = {
   [TYPES.TIMER]: '&nbsp;',
   [TYPES.KICK]: 'Kick',
   [TYPES.SNARE]: 'Snare',
@@ -15,10 +15,10 @@ const MAP_DISPLAY = {
 };
 
 class StepSequencer {
-  constructor(type, activeStepIds = []) {
+  constructor(type, tempo, activeStepIds = []) {
     this.type = type;
+    this.tempo = tempo;
     this.activeStepIds = activeStepIds;
-    this.tempo = 60;
     this.intervalId = null;
 
     // DOM Elements
@@ -26,25 +26,18 @@ class StepSequencer {
     this.stepWrapper = document.querySelector(`.step-wrapper.${this.type}`);
     this.stepElements = [];
 
-    this.mountElements();
-  }
-
-  mountElements() {
-    let dotHtmlCode = '&#8226;';
-
     // Append template clone to parent container and add title.
     this.stepWrapper.appendChild(this.stepTemplate.content.cloneNode(true));
-    this.stepWrapper.firstElementChild.innerHTML = MAP_DISPLAY[this.type];
+    this.stepWrapper.firstElementChild.innerHTML = MAP_DISPLAY_NAME[this.type];
     this.stepElements = this.stepWrapper.querySelectorAll('.step');
 
-    if (this.type !== TYPES.TIMER) {
-      this.activeStepIds.forEach(id => this.stepElements[id].innerHTML = dotHtmlCode);
-    } else {
-      Array.from(this.stepElements).forEach((item, index) => {
-        item.innerText = index + 1;
-        item.classList.remove('box');
-      });
-    }
+    this.populateSteps();
+  }
+
+  populateSteps() {
+    // Populate active step boxes with dots
+    let dotHtmlCode = '&#8226;';
+    this.activeStepIds.forEach(id => this.stepElements[id].innerHTML = dotHtmlCode);
   }
 
   startTempoAnimation() {
@@ -53,7 +46,7 @@ class StepSequencer {
 
     let indexCounter = 0;
     const stepInterval = 60/this.tempo*4/8*1000;  // in milliseconds
-    const stepsLength = this.stepElements.length;        // 16 in this case
+    const stepsLength = this.stepElements.length; // 16 in this case
 
     this.intervalId = setInterval(() => {
       const currIndex = indexCounter % stepsLength;
@@ -79,16 +72,27 @@ class StepSequencer {
   }
 }
 
+// Extend StepSequencer to build Timer
+class Timer extends StepSequencer {
+  populateSteps() {
+    Array.from(this.stepElements).forEach((item, index) => {
+      item.innerText = index + 1;   // populate with numbers instead of dots
+      item.classList.remove('box'); // unused class for timer
+    });
+  }
+}
+
 let arr0 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let arr1 = [1, 3, 5, 7, 9, 11, 13, 15];
 let arr2 = [0, 2, 4, 6, 8, 10, 12, 14];
 let arr3 = [3, 7, 11, 15];
+let tempo = 60;
 
-let timer = new StepSequencer(TYPES.TIMER);
-let kick = new StepSequencer(TYPES.KICK, arr1);
-let snare = new StepSequencer(TYPES.SNARE, arr2);
-let openHat = new StepSequencer(TYPES.OPEN_HAT, arr3);
-let closedHat = new StepSequencer(TYPES.CLOSED_HAT, arr0);
+let timer = new Timer(TYPES.TIMER, tempo);
+let kick = new StepSequencer(TYPES.KICK, tempo, arr1);
+let snare = new StepSequencer(TYPES.SNARE, tempo, arr2);
+let openHat = new StepSequencer(TYPES.OPEN_HAT, tempo, arr3);
+let closedHat = new StepSequencer(TYPES.CLOSED_HAT, tempo, arr0);
 
 let stopBtn = document.querySelector('.stop.btn');
 let playBtn = document.querySelector('.play.btn');
